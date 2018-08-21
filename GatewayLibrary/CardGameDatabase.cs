c95bcfd5cbb520fc58dcefe8f15ccdc3e9a97d27
Paul Power <paul.power@ni.com>
2018-08-21 01:02:52 -0500

    added ConsoleTest, CardGameDB Class
    I did a few preliminary tests with ConsoleTest app for my SQL server based class for communicating with NICardGame database.  All the class does at this point is open a SQL connection with the SQL Server on DTTest

---
+++ GatewayLibrary/CardGameDatabase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace GatewayLibrary
+{
+    /*the CardGameDatabase class connects 
+     * to the SQL Server to send/receive information 
+     * to and from the NICardGame database
+     */
+
+    public class CardGameDatabase
+    {
+       private static SqlConnection databaseConnection { get; set; }
+
+        public static void OpenDatabaseConnection()
+        {
+            //define connection string for connecting to the SQL Server database
+            string userID = "cardGame";
+            string password = "3as^3as^";
+            string server = "PAPOWER-DTTEST";
+            string database = "NICardGame";
+            string connectionString = $"user id={userID};password={password};" +
+                                      $"server={server};database={database};";
+            //connect to the NICardGame database
+            
+            databaseConnection = new SqlConnection(connectionString);
+            try
+            {
+                databaseConnection.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using GatewayLibrary;
+
+namespace TestConsole
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            CardGameDatabase database = new CardGameDatabase();
+            CardGameDatabase.OpenDatabaseConnection();
+
+            Console.ReadLine();
+        }
+    }
+}
